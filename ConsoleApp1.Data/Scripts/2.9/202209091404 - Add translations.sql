0f056dfaced32719318df72477951043b48385fb
JoasE <32096708+JoasE@users.noreply.github.com>
2022-09-09 14:27:53 +0200

    Add project files.

---
+++ ConsoleApp1.Data/Scripts/2.9/202209091404 - Add translations.sql
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Scripts._2._9
+{
+    class _202209091404___Add_translations
+    {
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp1.Data;
+using System.Reflection;
+
+var resourceNames = typeof(Class1).Assembly.GetManifestResourceNames();
+
+Console.ReadLine();
